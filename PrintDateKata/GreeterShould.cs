b5a7f9adb646a06cefb5ab51189f99f7cf77d589
panicoenlaxbox <panicoenlaxbox@gmail.com>
2017-09-27 11:48:59 +0200

    stub

--- PrintDateKata/GreeterShould.cs
+++ PrintDateKata/GreeterShould.cs
@@ -6,31 +6,57 @@ namespace PrintDateKata
 {
     public class GreeterShould
     {
-
         [Fact]
         public void greet()
         {
             var writer = new SpyWriter();
-            Greeter greeter = new Greeter(writer);
+            var now = DateTime.Now;
+            var dateTimeRetriever = new StubDateTimeRetriever(now);
+            var greeter = new Greeter(writer, dateTimeRetriever);
             greeter.Greet();
-            writer.Output.Should().StartWith("Hi, ");
+            writer.Output.Should().StartWith("Hi, " + now);
         }
     }
 
     public class Greeter
     {
         private readonly Writer _writer;
+        private readonly DateTimeRetriever _dateTimeRetriever;
 
-        public Greeter(Writer writer)
+        public Greeter(Writer writer, DateTimeRetriever dateTimeRetriever)
         {
             _writer = writer;
+            _dateTimeRetriever = dateTimeRetriever;
         }
 
         public void Greet()
         {
-            _writer.Write("Hi, " + DateTime.Now);
+            _writer.Write("Hi, " + _dateTimeRetriever.GetCurrent());
         }
     }
+
+    public class DateTimeRetriever
+    {
+        public virtual DateTime GetCurrent()
+        {
+            return DateTime.Now;
+        }
+    }
+
+    class StubDateTimeRetriever : DateTimeRetriever
+    {
+        private readonly DateTime _dateTime;
+
+        public StubDateTimeRetriever(DateTime dateTime)
+        {
+            _dateTime = dateTime;
+        }
+        public override DateTime GetCurrent()
+        {
+            return _dateTime;
+        }
+    }
+
 
     public class Writer
     {
